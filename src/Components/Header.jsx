47e7d84c5e7cb1dcb4c8b4271d8e0a93882e9746
lucassodresa <lucas.sodresa@gmail.com>
2022-02-19 16:06:44 +0000

    base components and initial structure

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <nav className="container">
+        <Link to="/">Home</Link>
+        <Link to="/login">Login / Register</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
