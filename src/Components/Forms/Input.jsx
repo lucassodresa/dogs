2dcfdf7ee7c77a0388843cfbf3f92f4ddeab466e
lucassodresa <lucas.sodresa@gmail.com>
2022-02-20 17:13:43 +0000

    improvements in Input component

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import styles from "./Input.module.css";
 import PropTypes from "prop-types";
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, error, onChange, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input id={name} name={name} className={styles.input} type={type} />
-      <p className={styles.error}>Error</p>
+      <input
+        id={name}
+        name={name}
+        className={styles.input}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      <p className={styles.error}>{error}</p>
     </div>
   );
 };
@@ -22,6 +30,10 @@ Input.propTypes = {
   label: PropTypes.string,
   type: PropTypes.string,
   name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  error: PropTypes.string,
+  onChange: PropTypes.func,
+  onBlur: PropTypes.func,
 };
 
 export default Input;
