ff18da4f56075c5808191de2c28de1f45ed1328a
lucassodresa <lucas.sodresa@gmail.com>
2022-03-09 02:52:33 +0000

    FeedPhotosItem component

--- src/Components/Feed/FeedPhotosItem.js
+++ src/Components/Feed/FeedPhotosItem.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useCallback } from "react";
 import PropTypes from "prop-types";
 import styles from "./FeedPhotosItem.module.css";
 
-const FeedPhotosItem = ({ photo }) => {
+const FeedPhotosItem = ({ photo, setModalPhoto }) => {
+  const handleClick = useCallback(
+    () => setModalPhoto(photo),
+    [photo, setModalPhoto]
+  );
   if (!photo) return null;
+
   const { src, title, acessos: views } = photo;
+
   return (
-    <li className={styles.photo}>
+    <li className={styles.photo} onClick={handleClick}>
       <img src={src} alt={title} />
       <span className={styles.views}>{views}</span>
     </li>
@@ -15,9 +21,11 @@ const FeedPhotosItem = ({ photo }) => {
 
 FeedPhotosItem.defaultProps = {
   photo: null,
+  setModalPhoto: () => {},
 };
 
 FeedPhotosItem.propTypes = {
   photo: PropTypes.object,
+  setModalPhoto: PropTypes.func,
 };
 export default FeedPhotosItem;
