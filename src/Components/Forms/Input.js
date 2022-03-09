dc4b0c08f041f43e8581cc8a21a49375fb30112f
lucassodresa <lucas.sodresa@gmail.com>
2022-03-09 02:30:12 +0000

    logic to accept textarea added

--- src/Components/Forms/Input.js
+++ src/Components/Forms/Input.js
@@ -2,21 +2,44 @@ import React from "react";
 import styles from "./Input.module.css";
 import PropTypes from "prop-types";
 
-const Input = ({ label, type, name, value, error, onChange, onBlur }) => {
+const Input = ({
+  label,
+  type,
+  name,
+  value,
+  error,
+  onChange,
+  onBlur,
+  className,
+}) => {
   return (
-    <div className={styles.wrapper}>
+    <div className={`${styles.wrapper} ${className}`}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input
-        id={name}
-        name={name}
-        className={styles.input}
-        type={type}
-        value={value}
-        onChange={onChange}
-        onBlur={onBlur}
-      />
+
+      {type === "textarea" ? (
+        <textarea
+          id={name}
+          name={name}
+          className={styles.input}
+          type={type}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+      ) : (
+        <input
+          id={name}
+          name={name}
+          className={styles.input}
+          type={type}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+      )}
+
       <p className={styles.error}>{error}</p>
     </div>
   );
