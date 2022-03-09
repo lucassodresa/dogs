f9148879e801bf459be5037b546285b8ce7bd27a
lucassodresa <lucas.sodresa@gmail.com>
2022-03-09 03:08:23 +0000

    FeedModal component

--- src/Components/Feed/FeedModal.js
+++ src/Components/Feed/FeedModal.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
+import PropTypes from "prop-types";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+import { PHOTO_GET } from "../../Utils/api";
 
-const FeedModal = () => {
-  return <div>FeedModal</div>;
+import styles from "./FeedModal.module.css";
+import PhotoContent from "../Photo/PhotoContent";
+
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  const handleOutsideClick = useCallback(
+    ({ target, currentTarget }) => {
+      if (target === currentTarget) setModalPhoto(null);
+    },
+    [setModalPhoto]
+  );
+
+  return (
+    <div className={styles.modal} onClick={handleOutsideClick}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
+};
+
+FeedModal.defaultProps = {
+  photo: null,
+};
+
+FeedModal.propTypes = {
+  photo: PropTypes.object,
 };
 
 export default FeedModal;
