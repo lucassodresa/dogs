231870cc43813667b60c6d9bead3bf8b4d4a24fc
lucassodresa <lucas.sodresa@gmail.com>
2022-03-09 03:06:31 +0000

    PhotoCommentsForm component

---
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -0,0 +1,51 @@
+import React, { useCallback } from "react";
+import { ReactComponent as SendIcon } from "../../Assets/send.svg";
+import commentSchema from "../../Constants/Schemas/commentSchema";
+import useFetch from "../../Hooks/useFetch";
+import useForm from "../../Hooks/useForm";
+import { COMMENT_POST } from "../../Utils/api";
+import Error from "../Helper/Error";
+import styles from "./PhotoCommentsForm.module.css";
+
+const PhotoCommentsForm = ({ id, setComments }) => {
+  const comment = useForm(commentSchema, "comment");
+  const { request, error } = useFetch();
+
+  const handleSubmit = useCallback(
+    async (event) => {
+      event.preventDefault();
+
+      const token = window.localStorage.getItem("token");
+      const { url, options } = COMMENT_POST(
+        id,
+        { comment: comment.value },
+        token
+      );
+      const { response, json } = await request(url, options);
+
+      if (response.ok) {
+        comment.setValue("");
+        setComments((comments) => [...comments, json]);
+      }
+    },
+    [comment, request, id, setComments]
+  );
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <textarea
+        className={styles.textarea}
+        id="comment"
+        name="comment"
+        placeholder="Bark..."
+        {...comment}
+      />
+      <button className={styles.button}>
+        <SendIcon />
+      </button>
+      <Error error={error} />
+    </form>
+  );
+};
+
+export default PhotoCommentsForm;
