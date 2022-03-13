e780d0ea811036e88668cb012f66da362532f720
lucassodresa <lucas.sodresa@gmail.com>
2022-03-13 09:54:24 +0000

    NotFound route added in Login component

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,6 +6,7 @@ import LoginForm from "./LoginForm";
 import LoginPasswordLost from "./LoginPasswordLost";
 import LoginPasswordReset from "./LoginPasswordReset";
 import styles from "./Login.module.css";
+import NotFound from "../NotFound";
 
 const Login = () => {
   const { isLoggedIn } = useContext(UserContext);
@@ -19,6 +20,7 @@ const Login = () => {
           <Route path="create" element={<LoginCreate />} />
           <Route path="lost" element={<LoginPasswordLost />} />
           <Route path="reset" element={<LoginPasswordReset />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </section>
