b625a8c634caba6cb07161bfc0455daa3a8228e0
lucassodresa <lucas.sodresa@gmail.com>
2022-03-13 14:54:19 +0000

    Footer component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,13 @@
 import styles from "./Footer.module.css";
+import { ReactComponent as DogsIcon } from "../Assets/dogs-footer.svg";
 
 const Footer = () => {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <DogsIcon />
+      <p>Dogs. Copyright © {new Date().getFullYear()}</p>
+    </footer>
+  );
 };
 
 export default Footer;
